1ba930f741d5f7b46866251b4dfb5b6358b5e633
Raul <raul.lucero27@gmail.com>
2015-01-04 09:55:55 -0600

    [add] Personaje principal con animaciones y acciones Salta,Caminar,Caida

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 cameraPos;
+	private Vector3 result;
+
+	public float smoothTime = 0.1f; // time for dampen
+	public bool cameraFollowX = true; // camera follows on horizontal
+	public Vector2 velocity; // speed of camera movement
+	private Transform thisTransform; // camera Transform
+
+	// Use this for initialization
+	void Start () {
+		thisTransform = transform;
+		velocity.x = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.Find ("Cubito").transform;
+		thisTransform.position = new Vector3(Mathf.SmoothDamp(thisTransform.position.x, player.transform.position.x, ref velocity.x, smoothTime), thisTransform.position.y, thisTransform.position.z);
+
+	}
+}
---
+++ Assets/Scripts/CubitoScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubitoScript : MonoBehaviour
+{
+
+	
+		public float maxSpeed = 5f;
+		bool facingRight = true;
+		Animator anim;
+		bool grounded = false;
+		public Transform groundCheck;
+		float groundRadius = 0.02f;
+		public LayerMask whatIsGround;
+		
+	
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+		}
+	
+		void FixedUpdate ()
+		{
+
+				//Vector3 pointCamera = camera.WorldToViewportPoint (transform.position);
+				//Debug.Log (pointCamera);		
+		
+		
+				grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+				anim.SetBool ("Ground", grounded);
+
+				anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+
+
+				float move = Input.GetAxis ("Horizontal");
+
+				anim.SetFloat ("Speed", Mathf.Abs (move));
+		
+				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		
+		
+				if (move > 0 && !facingRight)
+						Flip ();
+				else if (move < 0 && facingRight)
+						Flip ();
+		
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+				//camera.ViewportToWorldPoint (new Vector3 (rigidbody2D.velocity.x, 3,-10));
+				
+
+				if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+						anim.SetBool ("Ground", false);
+						anim.SetBool ("isJump", true);
+						rigidbody2D.AddForce (new Vector2 (0, 350f));
+				} else if (!grounded) {
+						anim.SetBool ("isJump", false);		
+				}
+			
+		
+		}
+	
+		void Flip ()
+		{
+				facingRight = !facingRight;
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+		}
+}
