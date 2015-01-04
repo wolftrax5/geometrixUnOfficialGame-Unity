a99c7dcc5b6a06a4b6b6eaaefe773291b078c6d5
Raul <raul.lucero27@gmail.com>
2015-01-04 15:58:32 -0600

    [add] Se agrega movimiento sobre el circulo en una area

--- Assets/Scripts/CirculoScript.cs
+++ Assets/Scripts/CirculoScript.cs
@@ -1,43 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CirculoScript : MonoBehaviour {
+public class CirculoScript : MonoBehaviour
+{
 
-	bool facingRight = true;
-	int direction;
-	float move;
-	Animator anim;
+		bool facingRight = true;
+		int direction;
+		float move;
+		Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		anim = GetComponent<Animator> ();
-	}
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+				move = -4f;
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
 
-	void FixedUpdate ()
-	{
-		move = -.7f;
-		anim.SetFloat ("Speed", Mathf.Abs (move));
-		rigidbody2D.velocity = new Vector2 (move, rigidbody2D.velocity.y);
+		//Debug.Log (transform.position.x+ " " +transform.position.y);
+		if (transform.position.x>10.6f && transform.position.x< 10.8f) {
+			move *= -1;
+			Flip();
+			Debug.Log("Cambio hacia derecha");
+			
+		} else if (transform.position.x>31.5 && transform.position.x< 31.7f) {
+			move *= -1;
+			Flip();
+			Debug.Log("Cambio hacia derecha");
+		}
+		}
+
+		void FixedUpdate ()
+		{
+
+				anim.SetFloat ("Speed", Mathf.Abs (move));
+				rigidbody2D.velocity = new Vector2 (move, rigidbody2D.velocity.y);
+
 		
-		
-		/*if (move > 0 && !facingRight) 
+				/*if (move > 0 && !facingRight) 
 			Flip ();
 		else if (move < 0 && facingRight) 
 			Flip ();*/
 		
 		
-	}
+		}
+
+
 	
-	void Flip ()
-	{
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+		void Flip ()
+		{
+				facingRight = !facingRight;
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+		}
 }
